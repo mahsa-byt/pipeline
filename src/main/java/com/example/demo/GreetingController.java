70b918af583a52b6372a71498f868809f2b6dae5
mahsa-byt <mahsa.byt@gmail.com>
2020-06-04 11:19:17 +0100

    First commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public @ResponseBody String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Model model) {
+        model.addAttribute("name", name);
+        return "Hiiiiiiiii";
+    }
+}
